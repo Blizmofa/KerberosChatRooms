391e59f05c88ff92d9c7cc8bac296d39758053d0
Blizmofa <blizmofa@gmail.com>
2024-01-16 22:51:06 +0200

    Msg Server Initial Commit

---
+++ msg_server.py
@@ -0,0 +1,17 @@
+
+"""Msg server main file"""
+
+
+def main():
+    pass
+
+    # Parse ip_address and port from msg.info file
+    # TODO - util method to parse ip and port from file. split(':'), validate pattern in line 1
+
+    # Create Msg Server Object
+
+    # MsgServer.run()
+
+
+if __name__ == "__main__":
+    main()
