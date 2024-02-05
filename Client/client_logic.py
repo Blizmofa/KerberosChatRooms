3689d4c65e720678b45e5c1b7de9322d1af766ac
Blizmofa <blizmofa@gmail.com>
2024-02-05 22:35:38 +0200

    Refactor AES Key request

--- Client/client_logic.py
+++ Client/client_logic.py
@@ -1,6 +1,7 @@
 import struct
 from typing import Union
 from Socket.custom_socket import CustomSocket
+from Server.MsgServer.msg_server_constants import Constants as MsgConsts
 from Utils.logger import Logger
 from Utils import utils
 from Utils.validator import Validator, Constants as ValConsts
@@ -8,6 +9,7 @@ from Utils.custom_exception_handler import CustomException
 from Protocol_Handler.protocol_handler import ProtocolHandler
 from Protocol_Handler.protocol_utils import ProtocolConstants as ProtoConsts, code_to_payload_template, server_request, server_response
 from Client.client_constants import Constants as CConsts, client_ram_template, me_info_default_data, file_db_servers_template
+from Client.client_input import ClientInput
 from Utils.encryptor import Encryptor
 
 
@@ -23,6 +25,7 @@ class ClientLogic(CustomSocket):
         self.logger = Logger(logger_name=self.__class__.__name__, debug_mode=debug_mode)
         self.protocol_handler = ProtocolHandler(debug_mode=debug_mode)
         self.encryptor = Encryptor(debug_mode=debug_mode)
+        self.client_input = ClientInput(debug_mode=debug_mode)
 
     def connect(self):
         """Setups the Msg Server as a client in order to register to Authentication server."""
@@ -93,27 +96,65 @@ class ClientLogic(CustomSocket):
             raise CustomException(error_msg=f"Unable to handle registration request for client "
                                             f"'{ram_template[CConsts.RAM_USERNAME]}'.", exception=e)
 
-    def __get_client_wanted_service(self) -> dict:
+    def __parse_msg_info_file_to_json(self, file_path: str, formatter: dict) -> dict:
+        # TODO - refactor, same method in auth server
+        if not utils.check_if_exists(path_to_check=file_path):
+            raise OSError(f"File '{file_path}' does not exists.")
+        default_service = utils.parse_info_file(file_path=file_path)
+        ip_and_port = default_service[MsgConsts.LINE_IP_PORT]
+        server_name = default_service[MsgConsts.LINE_NAME]
+        server_id = default_service[MsgConsts.LINE_ID]
+        server_ip, server_port = Validator.validate_injection(data_type=ValConsts.FMT_IPV4_PORT,
+                                                              value_to_validate=ip_and_port)
+        Validator.validate_injection(data_type=ValConsts.FMT_NAME, value_to_validate=server_name)
+        if not isinstance(server_id, bytes):
+            server_id = Validator.validate_injection(data_type=ValConsts.FMT_ID, value_to_validate=server_id)
+
+        formatter[CConsts.RAM_SERVER_ID] = server_id
+        formatter[CConsts.RAM_SERVER_NAME] = server_name
+        formatter[CConsts.RAM_SERVER_IP] = server_ip
+        formatter[CConsts.RAM_SERVER_PORT] = server_port
+        return formatter
+
+    def __get_client_wanted_service(self):
         """Private method that return the wanted service entry from Client DB."""
         try:
-            # msg_servers_list = {'server_id': b'2\xb0\x07o\xa4ODx\x90\xd6f\xa6\xa8\xf5\x94z', 'server_name': 'Printer 10', 'server_ip': '127.0.0.1', 'server_port': 56841}
-            # self.msg_servers_list = [{'server_id': b'2\xb0\x07o\xa4ODx\x90\xd6f\xa6\xa8\xf5\x94z', 'server_name': 'Printer 10', 'server_ip': '127.0.0.1', 'server_port': 56841}, {'server_id': b'j;\xd46)\x16O\xdd\x963a\x18t\xa5\x90z', 'server_name': 'Printer 20', 'server_ip': '127.0.0.1', 'server_port': 56846}, {'server_id': b'\xe9\xa8\x0c\xcf\t\x06O\x94\xb8d\xddP\x87\xc1\xd5\x9b', 'server_name': 'Printer 30', 'server_ip': '127.0.0.1', 'server_port': 56851}]
-            # if msg_servers_list:
-            server_name = "Printer 20"
-            if self.msg_servers_list:
-                # TODO - get wanted server name from client
-                msg_server = utils.fetch_value_from_ram_db(data=self.msg_servers_list,
-                                                          pivot_key=CConsts.RAM_SERVER_NAME,
-                                                          pivot_value=server_name)
+            # self.msg_servers_list = [{'server_id': b'\x8e\x811\xb0\xb6\xb7J\xe9\xb3T0!\xc7\xb2\xeb\xf6', 'server_name': 'Printer 10',
+            #   'server_ip': '127.0.0.1', 'server_port': 56673},
+            #  {'server_id': b'\xc9\xce*\xde\x0e\xe6MM\xb3\x08\x11\xf2+\x0b|\xfc', 'server_name': 'Printer 20',
+            #   'server_ip': '127.0.0.1', 'server_port': 56679},
+            #  {'server_id': b'\x04\xea\x9d\xc5\n\xc6CM\xb5\x88\xb4\x11\xd0\xd9\xcd\xa9', 'server_name': 'Printer 30',
+            #   'server_ip': '127.0.0.1', 'server_port': 56685},
+            #  {'server_id': b'\xd2\xfb3\x9c\xaauN\x9d\x8f\x95\xed\x05\xef\x8c\xc8\x06', 'server_name': 'Printer 40',
+            #   'server_ip': '127.0.0.1', 'server_port': 56687},
+            #  {'server_id': '\x00.JsM{nr)c\x16', 'server_name': 'Printer 50', 'server_ip': '127.0.0.1',
+            #   'server_port': 56693},
+            #  {'server_id': b'B3\xf8CP\x90O\xf8\xaf\t\xc4\xce\x8e^}J', 'server_name': 'Printer 60',
+            #   'server_ip': '127.0.0.1', 'server_port': 56699},
+            #  {'server_id': b'\xbb\x898\xe5\x15\xc0L\x14\x80\xcb`\xb5\x1dM7\xc8', 'server_name': 'Printer 70',
+            #   'server_ip': '127.0.0.1', 'server_port': 56704},
+            #  {'server_id': b'\xcfCH\x8d}\x1fM>\xb3\x98_(|\x11\xd3I', 'server_name': 'Printer 80',
+            #   'server_ip': '127.0.0.1', 'server_port': 56710}]
 
-                self.logger.logger.debug(f"Fetched server id {msg_server} from servers list successfully.")
-                return msg_server
+            # Services list is empty
+            if not self.msg_servers_list:
+                # TODO - maybe it should be srv_info.json.
+                self.msg_servers_list.append(self.__parse_msg_info_file_to_json(file_path=MsgConsts.MSG_FILE_NAME,
+                                                                                formatter=file_db_servers_template.copy()))
 
-            elif utils.check_if_exists(path_to_check=CConsts.SERVERS_FILE_NAME):
-                msg_server = utils.fetch_value_from_json_db(file_path=CConsts.SERVERS_FILE_NAME,
-                                                           pivot_key=CConsts.RAM_SERVER_NAME,
-                                                           pivot_value=server_name)
-                return msg_server
+            # Services file is empty
+            if not self.msg_servers_list:
+                print(utils.write_with_color(msg=f"{ProtoConsts.CONSOLE_ERROR} There aren't any registered services, "
+                                                 f"please call support.", color=utils.Colors.RED))
+
+            # get and return the wanted service object
+            server_object = self.client_input.get_service_name(services_list=self.msg_servers_list)
+
+            # For dev mode:
+            if self.debug_mode:
+                print(utils.write_with_color(msg=f"Selected service object --> {server_object}", color=utils.Colors.CYAN))
+
+            return server_object
 
         except Exception as e:
             raise CustomException(error_msg=f"Unable to get client wanted service.", exception=e)
@@ -127,18 +168,20 @@ class ClientLogic(CustomSocket):
             Validator.validate_injection(data_type=ValConsts.FMT_NONCE, value_to_validate=client_nonce)
 
             # Validate client id
-            client_id = ram_template[CConsts.RAM_ID]
+            client_id = utils.parse_info_file(file_path=CConsts.CLIENT_FILE_NAME, target_line_number=CConsts.CLIENT_ID_LINE)
+            # client_id = ram_template[CConsts.RAM_ID]
             if client_id and not isinstance(client_id, bytes):
                 client_id = Validator.validate_injection(data_type=ValConsts.FMT_ID, value_to_validate=client_id)
 
             # Get client wanted service and validate data
             msg_server = self.__get_client_wanted_service()
             server_id = msg_server[CConsts.RAM_SERVER_ID]
+
             if not isinstance(server_id, bytes):
                 server_id = Validator.validate_injection(data_type=ValConsts.FMT_ID, value_to_validate=server_id)
+            ram_template[CConsts.RAM_SERVER_ID] = server_id
 
             # Create packet data frame
-            # TODO - server id is the wanted msg server
             data = {
                 ProtoConsts.CLIENT_ID: client_id,
                 ProtoConsts.VERSION: ProtoConsts.SERVER_VERSION,
@@ -159,14 +202,21 @@ class ClientLogic(CustomSocket):
                                                          logger=self.logger)
 
             response_code, unpacked_aes_key_response = self.protocol_handler.unpack_request(received_packet=encrypted_key_response,
-                                                                           formatter=server_response_formatter,
-                                                                           code=1603,
-                                                                           deserialize=True)
+                                                                                            formatter=server_response_formatter,
+                                                                                            deserialize=True)
             # For dev mode
             if self.debug_mode:
                 print(utils.write_with_color(
                     msg=f"Received Response --> Code: {response_code}, Data: {unpacked_aes_key_response}",
                     color=utils.Colors.MAGENTA))
+
+            unpacked_encrypted_key = self.protocol_handler.unpack_request(received_packet=unpacked_aes_key_response[ProtoConsts.ENCRYPTED_KEY],
+                                                                          formatter=code_to_payload_template[ProtoConsts.PKT_ENCRYPTED_KEY])
+
+            # TODO - continue, decrypt key and iv and nonce, insert to ram. validate decrypted nonce with the client nonce
+
+            packet_ticket = unpacked_aes_key_response[ProtoConsts.TICKET]
+
 
 
             # # TODO - add to client ram template
@@ -219,7 +269,7 @@ class ClientLogic(CustomSocket):
     def handle_services_list_request(self, ram_template: dict, server_request_formatter: dict, server_response_formatter: dict) -> None:
         try:
             # Validate data
-            client_id = ram_template[CConsts.RAM_ID]
+            client_id = utils.parse_info_file(file_path=CConsts.CLIENT_FILE_NAME, target_line_number=CConsts.CLIENT_ID_LINE)
             if client_id and not isinstance(client_id, bytes):
                 client_id = Validator.validate_injection(data_type=ValConsts.FMT_ID,
                                                          value_to_validate=client_id)
@@ -263,7 +313,7 @@ class ClientLogic(CustomSocket):
             raise CustomException(error_msg=f"Unable to handle services list request.", exception=e)
 
     def run(self) -> None:
-
+        # TODO - fix, client is stuck after 1 request
         try:
             # Connect to Auth server
             self.connect()
@@ -276,8 +326,8 @@ class ClientLogic(CustomSocket):
             ram_template = client_ram_template.copy()
 
             if utils.check_if_exists(CConsts.CLIENT_FILE_PATH):
-                username = utils.parse_info_file(file_path=CConsts.CLIENT_FILE_PATH, target_line_number=1)
-                client_id = utils.parse_info_file(file_path=CConsts.CLIENT_FILE_PATH, target_line_number=2)
+                username = utils.parse_info_file(file_path=CConsts.CLIENT_FILE_PATH, target_line_number=CConsts.CLIENT_NAME_LINE)
+                client_id = utils.parse_info_file(file_path=CConsts.CLIENT_FILE_PATH, target_line_number=CConsts.CLIENT_ID_LINE)
 
             else:
                 username = input("Please enter your username: ")
@@ -297,30 +347,43 @@ class ClientLogic(CustomSocket):
             server_request_formatter = server_request.copy()
             server_response_formatter = server_response.copy()
 
-            # Handle registration request from Authentication Server
-            # self.handle_registration_request(ram_template=ram_template,
-            #                                  server_request_formatter=server_request_formatter,
-            #                                  server_response_formatter=server_response_formatter)
+            # Run client logic
+            # TODO - refactor, move to client input class, create a run method with Callable to call the method
+            while True:
+                client_input = self.client_input.show_client_menu()
 
-            # TODO - if already registered do something, maybe if server responded with an error
+                # Handle registration request to Authentication Server
+                if client_input == 1:
+                    self.handle_registration_request(ram_template=ram_template,
+                                                     server_request_formatter=server_request_formatter,
+                                                     server_response_formatter=server_response_formatter)
+                    # TODO - if already registered do something, maybe if server responded with an error
+                    # For dev mode
+                    if self.debug_mode:
+                        print(utils.write_with_color(msg=f"Registered client template --> {ram_template}", color=utils.Colors.CYAN))
 
-            # For dev mode
-            if self.debug_mode:
-                print(utils.write_with_color(msg=f"Registered client template --> {ram_template}", color=utils.Colors.CYAN))
+                # Handle services list request
+                elif client_input == 2:
+                    self.handle_services_list_request(ram_template=ram_template,
+                                                      server_request_formatter=server_request_formatter,
+                                                      server_response_formatter=server_response_formatter)
 
-            # Handle AES key request from Authentication Server
-            self.handle_aes_key_request(ram_template=ram_template,
-                                        server_request_formatter=server_request_formatter,
-                                        server_response_formatter=server_response_formatter)
-            # For dev mode
-            if self.debug_mode:
-                print(utils.write_with_color(msg=f"AES Key client template --> {ram_template}",
-                                             color=utils.Colors.CYAN))
+                    print(utils.write_with_color(msg=f"{ProtoConsts.CONSOLE_ACK} Services list has received successfully, "
+                                                     f"and been parse to '{CConsts.SERVERS_FILE_NAME}'", color=utils.Colors.GREEN))
 
-            # # Handle services list request
-            # self.handle_services_list_request(ram_template=ram_template,
-            #                                   server_request_formatter=server_request_formatter,
-            #                                   server_response_formatter=server_response_formatter)
+                # Handle AES Key request
+                elif client_input == 3:
+                    # Handle AES key request from Authentication Server
+                    self.handle_aes_key_request(ram_template=ram_template,
+                                                server_request_formatter=server_request_formatter,
+                                                server_response_formatter=server_response_formatter)
+                    # For dev mode
+                    if self.debug_mode:
+                        print(utils.write_with_color(msg=f"AES Key client template --> {ram_template}",
+                                                     color=utils.Colors.CYAN))
+
+                else:
+                    print(f"{ProtoConsts.CONSOLE_FAIL} Invalid option, please choose another: ")
 
         except Exception as e:
             raise CustomException(error_msg=f"Unable to run {self.__class__.__name__}.", exception=e)
