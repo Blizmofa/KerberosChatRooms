206d2b7a2b60ae7c20c994ed6ee49900706ecd86
Blizmofa <blizmofa@gmail.com>
2024-02-14 15:41:58 +0200

    Added client menu

--- Client/client_logic.py
+++ Client/client_logic.py
@@ -1,6 +1,9 @@
 import struct
-from typing import Union
-from Socket.custom_socket import CustomSocket
+import time
+from queue import Queue
+from typing import Union, Optional
+from threading import Lock
+from Socket.custom_socket import CustomSocket, Thread
 from Server.MsgServer.msg_server_constants import Constants as MsgConsts
 from Utils.logger import Logger
 from Utils import utils
@@ -16,12 +19,14 @@ from Utils.encryptor import Encryptor
 class ClientLogic(CustomSocket):
 
     def __init__(self, server_ip: str, server_port: int, connection_protocol: str, debug_mode: bool) -> None:
-        super().__init__(connection_protocol)
+        super().__init__(connection_protocol=connection_protocol, debug_mode=debug_mode)
         self.server_ip = server_ip
         self.server_port = server_port
         self.debug_mode = debug_mode
+        self.lock = Lock()
         self.msg_servers_list = []
-        self.client_socket = super().create_socket()
+        self.receive_queue = Queue()
+        self.client_socket = self.create_socket()
         self.logger = Logger(logger_name=self.__class__.__name__, debug_mode=debug_mode)
         self.protocol_handler = ProtocolHandler(debug_mode=debug_mode)
         self.encryptor = Encryptor(debug_mode=debug_mode)
@@ -64,6 +69,14 @@ class ClientLogic(CustomSocket):
                                                                          formatter=server_request_formatter)
             self.send_packet(sck=self.client_socket, packet=packed_register_request)
 
+            # For dev mode
+            if self.debug_mode:
+                print(utils.write_with_color(msg=f"Request formatter --> {server_request_formatter}", color=utils.Colors.BLUE))
+                print(utils.write_with_color(
+                    msg=f"Sent Request --> Code: {ProtoConsts.REQ_CLIENT_REG}, Data: {data}",
+                    color=utils.Colors.MAGENTA))
+
+
             # Receive register response, unpack and deserialize packet data
             register_response = self.receive_packet(sck=self.client_socket)
             response_code, unpacked_register_response = self.protocol_handler.unpack_request(received_packet=register_response,
@@ -71,6 +84,8 @@ class ClientLogic(CustomSocket):
                                                                                              deserialize=True)
             # For dev mode
             if self.debug_mode:
+                print(utils.write_with_color(msg=f"Response formatter --> {server_response_formatter}",
+                                             color=utils.Colors.BLUE))
                 print(utils.write_with_color(msg=f"Received Response --> Code: {response_code}, Data: {unpacked_register_response}", color=utils.Colors.MAGENTA))
 
             # Register success
@@ -210,17 +225,24 @@ class ClientLogic(CustomSocket):
                     msg=f"Received Response --> Code: {response_code}, Data: {unpacked_aes_key_response}",
                     color=utils.Colors.MAGENTA))
 
+            if response_code == ProtoConsts.RES_GENERAL_ERROR:
+                print(utils.write_with_color(msg=f"{ProtoConsts.CONSOLE_FAIL} {CConsts.SERVER_GENERAL_ERROR}", color=utils.Colors.RED))
+                return
+
             unpacked_encrypted_key = self.protocol_handler.unpack_request(received_packet=unpacked_aes_key_response[ProtoConsts.ENCRYPTED_KEY],
                                                                           formatter=code_to_payload_template[ProtoConsts.PKT_ENCRYPTED_KEY])
 
-            # TODO - continue, decrypt key and iv and nonce, insert to ram. validate decrypted nonce with the client nonce
+            # TODO - refactor into a method
+            encrypted_key_iv, encrypted_nonce, encrypted_aes_key = unpacked_encrypted_key
+            password_hash = ram_template[CConsts.RAM_PASSWORD_HASH]
+            decrypted_key = self.encryptor.decrypt(encrypted_value=encrypted_aes_key, decryption_key=password_hash, iv=encrypted_key_iv)
+            decrypted_nonce = self.encryptor.decrypt(encrypted_value=encrypted_nonce, decryption_key=password_hash, iv=encrypted_key_iv)
+            ram_template[CConsts.RAM_AES_KEY] = decrypted_key
+            ram_template[CConsts.RAM_ENCRYPTED_KEY_IV] = encrypted_key_iv
 
-            packet_ticket = unpacked_aes_key_response[ProtoConsts.TICKET]
-
-
-
-            # # TODO - add to client ram template
-            # print(unpacked['ticket'])
+            # Validate nonce and return ticket
+            if client_nonce == decrypted_nonce:
+                ram_template[CConsts.RAM_TICKET] = unpacked_aes_key_response[ProtoConsts.TICKET]
 
         except Exception as e:
             raise CustomException(error_msg=f"Unable to handle encrypted key request.", exception=e)
@@ -249,6 +271,7 @@ class ClientLogic(CustomSocket):
                 server_data.update(self.protocol_handler.serialize_packet_value(formatter=server_data_formatter,
                                                                                 data=server_data,
                                                                                 mode=ProtoConsts.DESERIALIZE))
+                # TODO - add to list file the server aes key, iv and all other needed fields, so in case of disconnection the data will be in DB and not only in RAM
                 self.msg_servers_list.append(server_data)
 
             # For dev mode:
@@ -285,6 +308,7 @@ class ClientLogic(CustomSocket):
             packed_services_list_request = self.protocol_handler.pack_request(code=ProtoConsts.REQ_MSG_SERVERS_LIST,
                                                                               data=data,
                                                                               formatter=server_request_formatter)
+
             self.send_packet(sck=self.client_socket, packet=packed_services_list_request)
 
             # Receive servers list response, unpack and deserialize packet data
@@ -312,6 +336,149 @@ class ClientLogic(CustomSocket):
         except Exception as e:
             raise CustomException(error_msg=f"Unable to handle services list request.", exception=e)
 
+    def __pack_authenticator_packet(self, ram_template: dict) -> bytes:
+        try:
+            # Create Authenticator
+            authenticator_iv = self.encryptor.generate_bytes_stream()
+            ram_template[CConsts.RAM_AUTH_IV] = authenticator_iv
+            client_id = ram_template[CConsts.RAM_ID]
+            if not isinstance(client_id, bytes):
+                client_id = Validator.validate_injection(data_type=ValConsts.FMT_ID, value_to_validate=client_id)
+            server_id = utils.generate_uuid()
+            # server_id = ram_template[CConsts.RAM_SERVER_ID]
+            if not isinstance(client_id, bytes):
+                server_id = Validator.validate_injection(data_type=ValConsts.FMT_ID, value_to_validate=server_id)
+
+            aes_key = self.encryptor.generate_bytes_stream(size=32)
+            # aes_key = ram_template[CConsts.RAM_AES_KEY]
+            encrypted_client_id = self.encryptor.encrypt(value=client_id, encryption_key=aes_key, iv=authenticator_iv)
+            encrypted_server_id = self.encryptor.encrypt(value=server_id, encryption_key=aes_key, iv=authenticator_iv)
+            creation_time = utils.time_now()
+            encrypted_creation_time = self.encryptor.encrypt(value=creation_time, encryption_key=aes_key, iv=authenticator_iv)
+
+            # Create packet data frame
+            authenticator_data = {
+                ProtoConsts.AUTHENTICATOR_IV: authenticator_iv,
+                ProtoConsts.VERSION: ProtoConsts.SERVER_VERSION,
+                ProtoConsts.CLIENT_ID: encrypted_client_id,
+                ProtoConsts.SERVER_ID: encrypted_server_id,
+                ProtoConsts.CREATION_TIME: encrypted_creation_time
+            }
+
+            # Pack authenticator packet
+            return self.protocol_handler.pack_request(code=ProtoConsts.PKT_AUTHENTICATOR,
+                                                      data=authenticator_data,
+                                                      formatter=code_to_payload_template[ProtoConsts.PKT_AUTHENTICATOR].copy())
+
+        except Exception as e:
+            raise CustomException(error_msg=f"Unable to pack authenticator packet.", exception=e)
+
+    def connect_to_service(self, ram_template: dict, server_request_formatter: dict, server_response_formatter: dict) -> None:
+        try:
+            authenticator = self.__pack_authenticator_packet(ram_template=ram_template)
+            ticket = b'\x18cL\xb5\x942\xb1J\xe2\xa3\x02\xe4q\x89\xd8h\xa5\xb1\xbb\x7f\xb2Y\xfeKp\xb5\x86\xb8\xdc\x1e\xf7\xc4b06/02/24]=\xcf\xe0\xa4\x9c,\xa3\xceZ\x1d\xf5\xb0\xda\x1c\xfbh4\xae\xc0\x90\xde|\xa9Q_\\\x1e\xff\x89\xec\xa2\x93\xab\xc8\x98\x98s\t\x13v.{-<R\xf6&y\twDH$\xad\xb4\x92\xe0\xbc&\xd8q>\x98\xbf\xb6\xc3N\x8a\xbf"\xe5Y\xb9\xd9\x0f\x88\x1fi\x95\xc2\xb8\xdb\x06\xc6\x89\xa6\xe7\xbd\x91\x05$\xfa\x8a\xda"'
+            # ticket = ram_template[CConsts.RAM_TICKET]
+            client_id = ram_template[CConsts.RAM_ID]
+            if not isinstance(client_id, bytes):
+                client_id = Validator.validate_injection(data_type=ValConsts.FMT_ID, value_to_validate=client_id)
+
+            # Create packet data frame
+            data = {
+                ProtoConsts.CLIENT_ID: client_id,
+                ProtoConsts.VERSION: ProtoConsts.SERVER_VERSION,
+                ProtoConsts.CODE: ProtoConsts.REQ_MSG_SERVERS_LIST,
+                ProtoConsts.PAYLOAD_SIZE: len(authenticator) + len(ticket),
+                ProtoConsts.AUTHENTICATOR: authenticator,
+                ProtoConsts.TICKET: ticket
+            }
+
+            # Adjust sizes
+            server_data_formatter = code_to_payload_template[ProtoConsts.REQ_MSG_SERVER_AES_KEY].copy()
+            server_data_formatter.update(self.protocol_handler.update_formatter_value(formatter=server_data_formatter,
+                                                                                      pivot_key=ProtoConsts.AUTHENTICATOR,
+                                                                                      pivot_value=ProtoConsts.SIZE,
+                                                                                      new_value=len(authenticator)))
+            server_data_formatter.update(self.protocol_handler.update_formatter_value(formatter=server_data_formatter,
+                                                                                      pivot_key=ProtoConsts.TICKET,
+                                                                                      pivot_value=ProtoConsts.SIZE,
+                                                                                      new_value=len(ticket)))
+            packed_service_aes_request = self.protocol_handler.pack_request(code=ProtoConsts.REQ_MSG_SERVER_AES_KEY,
+                                                                            data=data,
+                                                                            formatter=server_request_formatter)
+
+            # TODO - connect to msg server then send
+            # Send
+
+        except Exception as e:
+            raise CustomException(error_msg=f"Unable to connect to service.", exception=e)
+
+    def start_client(self, ram_template: dict) -> None:
+        try:
+            # Run client logic
+            while True:
+                # Prompt client menu
+                client_input = self.client_input.show_client_menu()
+
+                # Check if already registered
+                is_registered = ram_template[CConsts.RAM_IS_REGISTERED]
+
+                # Handle registration request to Authentication Server
+                if client_input == 1:
+                    if not is_registered:
+                        self.handle_registration_request(ram_template=ram_template,
+                                                         server_request_formatter=server_request.copy(),
+                                                         server_response_formatter=server_response.copy())
+                        # For dev mode
+                        if self.debug_mode:
+                            print(utils.write_with_color(msg=f"Registered client template --> {ram_template}", color=utils.Colors.CYAN))
+                    else:
+                        print(utils.write_with_color(msg=f"{ProtoConsts.CONSOLE_ERROR} You are already registered.",
+                                                     color=utils.Colors.GREEN))
+
+                # Handle services list request
+                elif client_input == 2:
+                    if is_registered:
+                        self.handle_services_list_request(ram_template=ram_template,
+                                                          server_request_formatter=server_request.copy(),
+                                                          server_response_formatter=server_response.copy())
+
+                        print(utils.write_with_color(msg=f"{ProtoConsts.CONSOLE_ACK} Services list has received successfully, "
+                                                         f"and been parse to '{CConsts.SERVERS_FILE_NAME}'", color=utils.Colors.GREEN))
+                    else:
+                        print(utils.write_with_color(msg=f"{ProtoConsts.CONSOLE_ERROR} Please register first.", color=utils.Colors.RED))
+
+                # Handle AES Key request
+                elif client_input == 3:
+                    if is_registered:
+                        self.handle_aes_key_request(ram_template=ram_template,
+                                                    server_request_formatter=server_request.copy(),
+                                                    server_response_formatter=server_response.copy())
+                        # For dev mode
+                        if self.debug_mode:
+                            print(utils.write_with_color(msg=f"AES Key client template --> {ram_template}",
+                                                         color=utils.Colors.CYAN))
+                    else:
+                        print(utils.write_with_color(msg=f"{ProtoConsts.CONSOLE_ERROR} Please register first.",
+                                                     color=utils.Colors.RED))
+
+                # Connect to MSG server
+                elif client_input == 4:
+                    if is_registered:
+                        self.connect_to_service(ram_template=ram_template,
+                                                server_request_formatter=server_request.copy(),
+                                                server_response_formatter=server_response.copy())
+                    else:
+                        print(utils.write_with_color(msg=f"{ProtoConsts.CONSOLE_ERROR} Please register first.",
+                                                     color=utils.Colors.RED))
+                else:
+                    print(f"{ProtoConsts.CONSOLE_FAIL} Invalid option, please choose another: ")
+
+                # print("[+] Processing request...")
+                # time.sleep(2)
+
+        except Exception as e:
+            raise CustomException(error_msg=f"Unable to start {self.__class__.__name__}.", exception=e)
+
     def run(self) -> None:
         # TODO - fix, client is stuck after 1 request
         try:
@@ -335,55 +502,19 @@ class ClientLogic(CustomSocket):
 
             ram_template[CConsts.RAM_USERNAME] = username
             ram_template[CConsts.RAM_ID] = client_id
-            ram_template[CConsts.RAM_PASSWORD] = "qwer1234"
-            # ram_template[CConsts.RAM_PASSWORD] = input("Please enter your password: ")
+            password = "qwer1234"
+            # password = input("Please enter your password: ")
+            ram_template[CConsts.RAM_PASSWORD] = password
+            ram_template[CConsts.RAM_PASSWORD_HASH] = self.encryptor.hash_password(password=password)
             ram_template[CConsts.RAM_IS_REGISTERED] = False
 
             # For dev mode
             if self.debug_mode:
                 print(utils.write_with_color(msg=f"Parsed client template --> {ram_template}", color=utils.Colors.CYAN))
 
-            # Create formatters templates
-            server_request_formatter = server_request.copy()
-            server_response_formatter = server_response.copy()
-
-            # Run client logic
-            # TODO - refactor, move to client input class, create a run method with Callable to call the method
-            while True:
-                client_input = self.client_input.show_client_menu()
-
-                # Handle registration request to Authentication Server
-                if client_input == 1:
-                    self.handle_registration_request(ram_template=ram_template,
-                                                     server_request_formatter=server_request_formatter,
-                                                     server_response_formatter=server_response_formatter)
-                    # TODO - if already registered do something, maybe if server responded with an error
-                    # For dev mode
-                    if self.debug_mode:
-                        print(utils.write_with_color(msg=f"Registered client template --> {ram_template}", color=utils.Colors.CYAN))
-
-                # Handle services list request
-                elif client_input == 2:
-                    self.handle_services_list_request(ram_template=ram_template,
-                                                      server_request_formatter=server_request_formatter,
-                                                      server_response_formatter=server_response_formatter)
-
-                    print(utils.write_with_color(msg=f"{ProtoConsts.CONSOLE_ACK} Services list has received successfully, "
-                                                     f"and been parse to '{CConsts.SERVERS_FILE_NAME}'", color=utils.Colors.GREEN))
-
-                # Handle AES Key request
-                elif client_input == 3:
-                    # Handle AES key request from Authentication Server
-                    self.handle_aes_key_request(ram_template=ram_template,
-                                                server_request_formatter=server_request_formatter,
-                                                server_response_formatter=server_response_formatter)
-                    # For dev mode
-                    if self.debug_mode:
-                        print(utils.write_with_color(msg=f"AES Key client template --> {ram_template}",
-                                                     color=utils.Colors.CYAN))
-
-                else:
-                    print(f"{ProtoConsts.CONSOLE_FAIL} Invalid option, please choose another: ")
+            # self.start_client(ram_template=ram_template, server_request_formatter=server_request_formatter, server_response_formatter=server_response_formatter)
+            client_thread = Thread(target=self.start_client, args=(ram_template, ))
+            client_thread.start()
 
         except Exception as e:
             raise CustomException(error_msg=f"Unable to run {self.__class__.__name__}.", exception=e)
