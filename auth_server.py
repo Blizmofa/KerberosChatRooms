9a9f948a26b55963e3a8b71beef8885e1112cf66
Blizmofa <blizmofa@gmail.com>
2024-02-29 22:05:26 +0200

    Auth Server main refactor

--- auth_server.py
+++ auth_server.py
@@ -1,31 +1,69 @@
 from sys import exit as sys_exit
 from Utils import utils
-from Server.AuthServer.auth_server_constants import Constants
-from Server.AuthServer.auth_server_core import AuthServer
+from Utils.validator import Validator, ValConsts
+from Utils.custom_arg_parser import CustomArgParser, Namespace
+from Protocol_Handler.protocol_utils import ProtoConsts as ProtoConsts
+from Server.AuthServer.auth_server_constants import AuthConsts
+from Server.AuthServer.auth_server_core import AuthServerCore
+
+
+# TODO - after refactor, validate that register client fetching from db and not if "name"
+
+
+def get_kdc_args() -> Namespace:
+    """Returns a Namespace with the KDC parsed CLI arguments."""
+    kdc_parser = CustomArgParser(description="KDC argument parser.\n"
+                                             f"Example Usage: python3 [path_to_kdc] -d -pr "
+                                             f"{ProtoConsts.PROTO_TCP} -ip {ProtoConsts.LOCAL_HOST} -p {ProtoConsts.DEF_PORT_NUM}",
+                                 supress_usage=True)
+    kdc_parser.add_arg('-d', '--debug-mode', action='store_true',
+                       help=kdc_parser.format_arg_help(general_description="For more informative logs and print statements.",
+                                                       usage_example=f"python3 [path_to_kdc] -d/--debug_mode",
+                                                       default_value="False"))
+    kdc_parser.add_arg('-pr', '--protocol', type=str, nargs='?', default=ProtoConsts.PROTO_TCP,
+                       help=kdc_parser.format_arg_help(general_description="For the KDC connection protocol.",
+                                                       usage_example="python3 [path_to_kdc] -pr/--protocol [udp | tcp]"))
+    kdc_parser.add_arg('-ip', '--ip-address', type=str, nargs='?', default=ProtoConsts.LOCAL_HOST,
+                       help=kdc_parser.format_arg_help(general_description="For the KDC IP Address.",
+                                                       usage_example="python3 [path_to_kdc] -ip/--ip-address [ip_address]"))
+    kdc_parser.add_arg('-p', '--port', type=int, nargs='?', default=ProtoConsts.DEF_PORT_NUM,
+                       help=kdc_parser.format_arg_help(general_description="For the KDC Port number.",
+                                                       usage_example="python3 [path_to_client] -p/--port [port_number]"))
+    return kdc_parser.parse_args()
 
 
 def main():
 
     # Validate port.info file
-    if not utils.check_if_exists(Constants.PORT_FILE_NAME):
-        utils.create_port_file(file_name=Constants.PORT_FILE_NAME, port_num=str(Constants.PORT_DEFAULT_NUM))
-
-    # TODO - refactor into utils method
-    port_num = None
-    try:
-        port_num = utils.get_port_num(Constants.PORT_FILE_NAME)
-
-    except Exception as e:
-        print(e)
-        port_num = Constants.PORT_DEFAULT_NUM
+    if not utils.is_exists(AuthConsts.PORT_FILE_PATH):
+        utils.create_info_file(file_name=AuthConsts.PORT_FILE_PATH,
+                               file_data={ValConsts.FMT_PORT: ProtoConsts.DEF_PORT_NUM})
 
     try:
-        auth_server = AuthServer(connection_protocol="tcp", ip_address="127.0.0.1",
-                                 port=8000, debug_mode=False)
+        # Get port number from file
+        port_num = utils.parse_info_file(file_path=AuthConsts.PORT_FILE_PATH, target_line_number=1)
+
+    except ValueError:
+        # Get default port number
+        port_num = AuthConsts.PORT_DEFAULT_NUM
+
+    # Validate port number
+    Validator.validate_injection(data_type=ValConsts.FMT_PORT, value_to_validate=int(port_num))
+
+    try:
+        # Get CLI args
+        kdc_args = get_kdc_args()
+
+        # Create and run the KDC
+        auth_server = AuthServerCore(connection_protocol=kdc_args.protocol,
+                                     ip_address=kdc_args.ip_address,
+                                     port=int(port_num),
+                                     debug_mode=kdc_args.debug_mode)
         auth_server.run()
+
     except Exception as e:
         print(e)
-        sys_exit(1)
+        sys_exit(ProtoConsts.STATUS_ERROR_CODE)
 
 
 if __name__ == "__main__":
